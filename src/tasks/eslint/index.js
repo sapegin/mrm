af13efd52b419f693a036d22573e6c5f5034fd69
Artem Sapegin <artem@sapegin.ru>
2017-08-28 22:01:32 +0200

    Feat(eslint): TypeScript support

--- src/tasks/eslint/index.js
+++ src/tasks/eslint/index.js
@@ -1,29 +1,55 @@
 'use strict';
 
 const minimist = require('minimist');
-const { json, packageJson, install, uninstall } = require('mrm-core');
+const { json, packageJson, lines, install, uninstall } = require('mrm-core');
 
+const ignores = ['node_modules/'];
 const oldPackages = ['jslint', 'jshint'];
 
 module.exports = function(config) {
+	let exts = '';
 	const preset = config('eslintPreset', 'eslint:recommended');
 	const packages = config('eslintPeerDependencies', []);
+	const rules = config('eslintRules');
 	packages.push('eslint');
 	if (preset !== 'eslint:recommended') {
 		packages.push(`eslint-config-${preset}`);
 	}
 
-	// .eslintrc
-	const eslintrc = json('.eslintrc');
-	if (!eslintrc.get('extends', '').startsWith(preset)) {
-		eslintrc.set('extends', preset).save();
-	}
-
-	// package.json
 	const pkg = packageJson();
 
+	// .eslintrc
+	const eslintrc = json('.eslintrc');
+	if (rules) {
+		eslintrc.merge({ rules });
+	}
+	if (!eslintrc.get('extends', '').startsWith(preset)) {
+		eslintrc.set('extends', preset);
+	}
+
+	// TypeScript
+	if (pkg.get('devDependencies.typescript')) {
+		const parser = 'typescript-eslint-parser';
+		packages.push(parser);
+		eslintrc.merge({
+			parser,
+			rules: rules || {
+				// Disable rules not supported by TypeScript parser
+				// https://github.com/eslint/typescript-eslint-parser#known-issues
+				'no-undef': 0,
+				'no-unused-vars': 0,
+				'no-useless-constructor': 0,
+			},
+		});
+		exts = ' --ext .{ts,tsx}';
+	}
+
+	eslintrc.save();
+
+	// .eslintignore
+	lines('.eslintignore').add(ignores).save();
+
 	// Keep custom extensions
-	let exts = '';
 	const lintScript = pkg.getScript('lint');
 	if (lintScript) {
 		const args = minimist(lintScript.split(' ').slice(1));
