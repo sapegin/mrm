063650f94a106a2acc15e1f4c462c0f6658e9019
Artem Sapegin <artem@sapegin.ru>
2020-05-21 11:07:29 +0200

    Disable CodeFund

--- site/src/theme/DocSidebar/index.js
+++ site/src/theme/DocSidebar/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import useScript from '@charlietango/use-script';
 import DocSidebarBase from '@theme-original/DocSidebar';
 import styles from './styles.module.css';
 
-function DocSidebar(props) {
+function Extra() {
 	useScript('https://app.codefund.io/properties/816/funder.js');
+	return <div id="codefund" className={styles.extra}></div>;
+}
 
+function DocSidebar({ showExtra, ...props }) {
 	return (
 		<div className={styles.sidebar}>
 			<DocSidebarBase {...props} />
-			<div id="codefund" className={styles.extra}></div>
+			{showExtra && <Extra />}
 		</div>
 	);
 }
+
+DocSidebar.propTypes = {
+	showExtra: PropTypes.bool,
+};
+
+DocSidebar.defaultProps = {
+	showExtra: false,
+};
 
 export default DocSidebar;
