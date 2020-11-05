6453fbcba3dc82d6a56c4a65c8250de0b2f4f28a
Artem Sapegin <artem@sapegin.ru>
2020-11-05 19:35:46 +0100

    feat(prettier): Add overrides for various JSON files that should use spaces

--- packages/mrm-task-prettier/index.js
+++ packages/mrm-task-prettier/index.js
@@ -24,6 +24,12 @@ const defaultOverrides = [
 			proseWrap: 'never',
 		},
 	},
+	{
+		files: '*.{json,babelrc,eslintrc,remarkrc}',
+		options: {
+			useTabs: false,
+		},
+	},
 ];
 const defaultPrettierOptions = {
 	printWidth: 80,
