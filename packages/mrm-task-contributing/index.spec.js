c85ed6a43121f4f72f202935ad7b6ccb4fbdb011
Brett Zamir <brettz9@yahoo.com>
2021-02-07 12:12:52 +0800

    test(contributing): check bad name and email (full coverage)

--- packages/mrm-task-contributing/index.spec.js
+++ packages/mrm-task-contributing/index.spec.js
@@ -15,7 +15,44 @@ const stringify = o => JSON.stringify(o, null, '  ');
 
 afterEach(() => vol.reset());
 
+it('should throw when bad `packageName`', () => {
+	expect(
+		getTaskOptions(task, false, {
+			packageName: '',
+			github: 'gendalf',
+		})
+	).rejects.toThrow('Missing required config options: packageName.');
+});
+
+it('should throw when bad `github`', () => {
+	expect(
+		getTaskOptions(task, false, {
+			github: '',
+		})
+	).rejects.toThrow('Missing required config options: github, packageName.');
+});
+
 it('should add a Contributing.md file', async () => {
+	vol.fromJSON({
+		[`${__dirname}/templates/Contributing.md`]: fs
+			.readFileSync(path.join(__dirname, 'templates/Contributing.md'))
+			.toString(),
+		'/package.json': stringify({
+			name: 'unicorn',
+		}),
+	});
+
+	task(
+		await getTaskOptions(task, false, {
+			github: 'gendalf',
+		})
+	);
+
+	expect(
+		omitBy(vol.toJSON(), (v, k) => k.startsWith(__dirname))
+	).toMatchSnapshot();
+
+	// Ensure repeated access works
 	vol.fromJSON({
 		[`${__dirname}/templates/Contributing.md`]: fs
 			.readFileSync(path.join(__dirname, 'templates/Contributing.md'))
