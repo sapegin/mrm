5df525dd564d2c44aa8aef40d51f1e95a2256113
Brett Zamir <brettz9@yahoo.com>
2021-02-12 00:09:40 +0800

    Support pnpm-lock.yaml addition/removal in gitignore;

--- packages/mrm-task-gitignore/index.js
+++ packages/mrm-task-gitignore/index.js
@@ -14,15 +14,24 @@ module.exports = function task() {
 		'*.log',
 	];
 
-	// If project uses npm, ignore yarn.lock
+	// If project uses npm, ignore yarn.lock and pnpm-lock.yaml
 	if (fs.existsSync('package-lock.json')) {
 		add.push('yarn.lock');
+		add.push('pnpm-lock.yaml');
 		remove.push('package-lock.json');
 	}
 
-	// If project uses Yarn, ignore package-lock.json
+	// If project uses Yarn, ignore package-lock.json and pnpm-lock.yaml
 	if (fs.existsSync('yarn.lock')) {
 		remove.push('yarn.lock');
+		add.push('package-lock.json');
+		add.push('pnpm-lock.yaml');
+	}
+
+	// If project uses pnpm, ignore package-lock.json and yarn.lock
+	if (fs.existsSync('pnpm-lock.yaml')) {
+		remove.push('pnpm-lock.yaml');
+		add.push('yarn.lock');
 		add.push('package-lock.json');
 	}
 
